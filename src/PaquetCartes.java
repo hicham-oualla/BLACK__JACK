5d8270160cb4df7c28e39a703bdb0049eb16f7fa
hicham-oualla <hich2022am.oualla@gmail.com>
2024-02-16 16:25:24 +0100

    creation des classes

--- src/PaquetCartes.java
+++ src/PaquetCartes.java
@@ -1,2 +1,9 @@
+import java.util.Random;
+
 public class PaquetCartes {
+//random pour melenger les cartes
+private static Random random = new Random();
+        //=======================================================================
+        private static String[] typeArr = {"Cœur", "Pique", "Carreau", "Trèfle"};
+        private static String[] valueArr = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "KING", "QUEEN"};
 }

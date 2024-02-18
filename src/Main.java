76698aec06ce5e3f91df0b25cd4e496b98dabdaf
hicham-oualla <hich2022am.oualla@gmail.com>
2024-02-18 22:25:45 +0100

    cree la class du paquetcart et sa method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+        PaquetCartes paquet = new PaquetCartes();
+
+        // Tirer plusieurs cartes et afficher les résultats
+      //  for (int i = 0; i < 2; i++) {
+            String card = paquet.randomCardPlayer();
+            System.out.println("Carte tirée : " + card);
+            System.out.println("Total Value: " + paquet.getTotalValuePlayer());
+            System.out.println();
+        //}
+    }
 }
-
-
--- src/PaquetCartes.java
+++ src/PaquetCartes.java
@@ -1,9 +1,45 @@
 import java.util.Random;
 
 public class PaquetCartes {
-//random pour melenger les cartes
-private static Random random = new Random();
-        //=======================================================================
-        private static String[] typeArr = {"Cœur", "Pique", "Carreau", "Trèfle"};
-        private static String[] valueArr = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "KING", "QUEEN"};
+    private Random random = new Random();
+    private String[] typeArr = {"Cœur", "Pique", "Carreau", "Trèfle"};
+    private String[] valueArr = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",  "JACK", "KING", "QUEEN"};
+
+    private int totalValuePlayer;
+
+    public PaquetCartes() {
+
+        totalValuePlayer = 0;
+    }
+
+    public String randomCardPlayer() {
+        int typeRandom = random.nextInt(typeArr.length);
+        int valueRandom = random.nextInt(valueArr.length);
+
+        int cardValue = getValueAsNumber(valueArr[valueRandom]);
+
+        totalValuePlayer += cardValue;
+
+        return typeArr[typeRandom] + " " + valueArr[valueRandom] + " (Value: " + cardValue + ")";
+    }
+
+    public int getTotalValuePlayer() {
+        return totalValuePlayer;
+    }
+
+    public int getValueAsNumber(String value) {
+        if (value.equals("Ace")) {
+            if (totalValuePlayer > 21) {
+                return 1;
+            } else {
+                return 11;
+            }
+        } else if (value.equals("JACK") || value.equals("KING") || value.equals("QUEEN")) {
+            return 10;
+        } else {
+            return Integer.parseInt(value);
+        }
+    }
+
+
 }
